8a7518bccbc1bf3eb4afd488439d02dce22b5e09
Leng Vongchanh <leng.vongchanh@boeing.com>
2018-12-10 16:16:19 +1100

    Clang formatting

--- costmap_2d/src/layered_costmap.cpp
+++ costmap_2d/src/layered_costmap.cpp
@@ -118,8 +118,9 @@ void LayeredCostmap::updateMap(double robot_x, double robot_y, double robot_yaw)
 
         if (minx_ > prev_minx || miny_ > prev_miny || maxx_ < prev_maxx || maxy_ < prev_maxy)
         {
-            ROS_WARN_THROTTLE(1.0, "Illegal bounds change, was [tl: (%f, %f), br: (%f, %f)], but "
-                                   "is now [tl: (%f, %f), br: (%f, %f)]. The offending layer is %s",
+            ROS_WARN_THROTTLE(1.0,
+                              "Illegal bounds change, was [tl: (%f, %f), br: (%f, %f)], but "
+                              "is now [tl: (%f, %f), br: (%f, %f)]. The offending layer is %s",
                               prev_minx, prev_miny, prev_maxx, prev_maxy, minx_, miny_, maxx_, maxy_,
                               (*plugin)->getName().c_str());
         }
