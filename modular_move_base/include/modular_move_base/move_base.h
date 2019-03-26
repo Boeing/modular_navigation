50cb255cac92600aa2b70a62500bc0d0dd0f1f1e
Jason Cochrane <jason.cochrane@boeing.com>
2019-03-26 15:17:28 +1100

    Clang

--- modular_move_base/include/modular_move_base/move_base.h
+++ modular_move_base/include/modular_move_base/move_base.h
@@ -25,9 +25,9 @@
 
 #include <modular_move_base/Plan.h>
 
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 #include <condition_variable>
 #include <mutex>
