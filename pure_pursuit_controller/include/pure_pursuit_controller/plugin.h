6e82a397217601cfab3a46892d12678dbbd48699
Phillip Haeusler <phillip.haeusler@boeing.com>
2019-06-26 14:53:20 +1000

    Update defaults for velocity limits Check collisions for current pose and target pose Check for NaNs

--- pure_pursuit_controller/include/pure_pursuit_controller/plugin.h
+++ pure_pursuit_controller/include/pure_pursuit_controller/plugin.h
@@ -37,11 +37,11 @@ class PurePursuitController : public navigation_interface::Controller
     ros::SteadyTime last_update_;
 
     // Parameters
-    double look_ahead_ = 0.2;
+    double look_ahead_ = 0.1;
 
     double max_velocity_x_ = 0.20;
-    double max_velocity_y_ = 0.10;
-    double max_velocity_w_ = 0.2;
+    double max_velocity_y_ = 0.05;
+    double max_velocity_w_ = 0.1;
 
     double max_acceleration_x_ = 0.1;
     double max_acceleration_y_ = 0.1;
@@ -49,12 +49,14 @@ class PurePursuitController : public navigation_interface::Controller
 
     double goal_radius_ = 0.1;
 
+    double robot_radius_ = 0.5;
+
     double xy_goal_tolerance_ = 0.002;
     double yaw_goal_tolerance_ = 0.01;
 
-    Eigen::Vector3d goal_p_gain_ = {10.0, 10.0, 10.0};
+    Eigen::Vector3d goal_p_gain_ = {2.0, 2.0, 2.0};
     Eigen::Vector3d goal_i_gain_ = {0.0, 0.0, 0.0};
-    Eigen::Vector3d goal_d_gain_ = {0.2, 0.2, 0.2};
+    Eigen::Vector3d goal_d_gain_ = {0.0, 0.0, 0.0};
 
     Eigen::Vector3d control_error_;
     Eigen::Vector3d control_integral_;
