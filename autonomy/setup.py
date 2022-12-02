bdea5277dbca708b334e3690008a86fabd3eb279
franbesq <francisco.blanco@tecnalia.com>
2022-12-02 13:59:44 +0100

    Added executables on CMakeLists and setup files

--- autonomy/setup.py
+++ autonomy/setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')),
     ],
     install_requires=['setuptools'],
     author='Boeing',
